9a1bec2850e2ad0eca6b467ab85f9acae38c84c2
LiR4 <pedrolira521@gmail.com>
2025-01-16 01:51:52 -0300

    feat: add model

---
+++ src/main/java/com/api/model/BiFile.java
@@ -0,0 +1,26 @@
+package com.api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "bi_file")
+public class BiFile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String pathFile;
+    private String fileLocalDateTime;
+
+}
---
+++ src/main/java/com/api/model/BiFileResponse.java
@@ -0,0 +1,16 @@
+package com.api.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class BiFileResponse {
+    private Long id;
+
+    private String pathFile;
+    private String fileLocalDateTime;
+}
