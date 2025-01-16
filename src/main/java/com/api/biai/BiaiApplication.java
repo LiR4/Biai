8694fbd6490a6cdfb2f1a1929a8d78e4f6773211
LiR4 <pedrolira521@gmail.com>
2025-01-16 01:51:02 -0300

    init project

---
+++ src/main/java/com/api/biai/BiaiApplication.java
@@ -0,0 +1,13 @@
+package com.api.biai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiaiApplication.class, args);
+	}
+
+}
